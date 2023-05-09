f2e966542fc7e3d71f8b70b1b24713fc3bb7e394
caramel <AlexanderForward149@gmail.com>
2023-05-08 21:35:23 -0400

    Casting

---
+++ casting/cast.java
@@ -0,0 +1,19 @@
+public class cast {
+  
+  public static void main(String[] args) {
+        double myDouble = 112.35;
+        int myInt = (int)myDouble;
+        System.out.println("The double value: " + myDouble);
+        System.out.println("The integer value: " + myInt);
+
+        String myString = "49";
+        int newInt = Integer.parseInt(myString); 
+
+        System.out.println("The string value: " + myString);
+        System.out.println("The integer value: " + newInt);
+
+
+
+
+
+  }}
