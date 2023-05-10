c2cc2e62aeba7133428c783c8ff57976596f1919
caramel <AlexanderForward149@gmail.com>
2023-05-10 13:29:31 -0400

    mehtod man

--- Loops/StopAtFive/src/StopAtFive.java
+++ Loops/StopAtFive/src/StopAtFive.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class StopAtFive {
     public static void main(String[] args) {
 
-2
+        Scanner     in1     =       new Scanner(System.in);
+        System.out.println("Type in 5");
+        int      take     =        in1.nextInt();
+        int      yes         =       5;
+
+        while (take != yes){
+            Scanner     in2    =       new Scanner(System.in);
+            System.out.println("Dude pick 5!!");
+            take     =        in2.nextInt();
+        }
+        System.out.println("Good job picking 5");
+
     }
 }
